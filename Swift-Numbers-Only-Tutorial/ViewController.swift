a0e261fd551e54abaf9779aee124f765cead6fbc
Rick Wilson <rick@teamwilson.org>
2015-04-14 13:16:24 -0600

    Added a UITextField to the StoryBoard the numbers only code to the Controller

--- Swift-Numbers-Only-Tutorial/ViewController.swift
+++ Swift-Numbers-Only-Tutorial/ViewController.swift
@@ -8,17 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
+    @IBOutlet weak var NumbersOnlyTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NumbersOnlyTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+        func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+            var result = true
+            let prospectiveText = (textField.text as NSString).stringByReplacingCharactersInRange(range, withString: string)
+    
+            if textField == NumbersOnlyTextField {
+                if count(string) > 0 {
+                    let disallowedCharacterSet = NSCharacterSet(charactersInString: "0123456789.").invertedSet
+                    let replacementStringIsLegal = string.rangeOfCharacterFromSet(disallowedCharacterSet) == nil
+    
+                    let resultingStringLengthIsLegal = count(prospectiveText) <= 9
+    
+                    let scanner = NSScanner(string: prospectiveText)
+                    let resultingTextIsNumeric = scanner.scanDecimal(nil) && scanner.atEnd
+    
+                    result = replacementStringIsLegal &&
+                        resultingStringLengthIsLegal &&
+                    resultingTextIsNumeric
+                }
+            }
+            return result
+        }
 
 
 }
